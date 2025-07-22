2f710fd6ed94b1fb60d1c007e0719503ac515536
wangzaijun <wzjhelloworld@qq.com>
2025-07-22 05:19:15 +0000

    fix

--- lightllm/server/visualserver/model_infer/model_rpc.py
+++ lightllm/server/visualserver/model_infer/model_rpc.py
@@ -96,7 +96,7 @@ class VisualModelRpcServer(rpyc.Service):
         all_img_embeds = all_img_embeds.to(torch.device("cpu"))
 
         if self.tp_rank_id == 0:
-            ready_flags = self.cache_client.root.get_items_embed(uuids)
+            ready_flags = obtain(self.cache_client.root.get_items_embed(uuids))
             ids_to_set = []
             for i, ready in enumerate(ready_flags):
                 if ready:
