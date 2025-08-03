acb6aded91e18ec58b9aa173e3376c6431844c28
wangzaijun <wzjhelloworld@qq.com>
2025-08-03 10:35:26 +0800

    add kv_cache_utils.py

--- lightllm/utils/kv_cache_utils.py
+++ lightllm/utils/kv_cache_utils.py
@@ -1,3 +1,7 @@
+import os
+import ctypes
+import dataclasses
+from functools import lru_cache
 from lightllm.utils.envs_utils import get_env_start_args
 from lightllm.utils.log_utils import init_logger
 from lightllm.utils.config_utils import get_config_json
@@ -5,7 +9,8 @@ from lightllm.utils.config_utils import get_config_json
 logger = init_logger(__name__)
 
 
-def calcu_cpu_cache_page_num() -> int:
+@lru_cache(maxsize=None)
+def calcu_cpu_cache_meta() -> "CpuKVCacheMeta":
     args = get_env_start_args()
     assert args.enable_cpu_cache
     model_config = get_config_json(args.model_dir)
@@ -16,14 +21,96 @@ def calcu_cpu_cache_page_num() -> int:
 
     one_token_byte_size = layer_num * num_key_value_heads * head_dim * item_size
     cpu_cache_page_num = int((args.cpu_cache_storage_size * 1024 * 1024 * 1024) / one_token_byte_size)
-    return cpu_cache_page_num
+
+    cpu_cache_meta = CpuKVCacheMeta(
+        page_num=cpu_cache_page_num,
+        layer_num=layer_num,
+        num_heads=num_key_value_heads,
+        head_dim=head_dim,
+        item_size=item_size,
+    )
+
+    return cpu_cache_meta
 
 
-def get_cpu_cache_pin_tensor():
+@lru_cache(maxsize=None)
+def get_shm_kv_cache_ptr():
+    args = get_env_start_args()
+
+    # 加载 libc
+    libc = ctypes.CDLL("/usr/lib/x86_64-linux-gnu/libc.so.6")
+
+    # 设置 shmget 函数的参数类型和返回类型
+    libc.shmget.argtypes = (ctypes.c_long, ctypes.c_size_t, ctypes.c_int)
+    libc.shmget.restype = ctypes.c_int
+
+    # 设置 shmat 函数的参数类型和返回类型
+    libc.shmat.argtypes = (ctypes.c_int, ctypes.c_void_p, ctypes.c_int)
+    libc.shmat.restype = ctypes.c_void_p
+
+    # 创建共享内存
+    key = args.cpu_kv_cache_shm_id  # 共享内存的键
+    size = 1024  # 共享内存大小
+    shmflg = 0o666 | 0o1000  # 权限和 IPC_CREAT 标志
+
+    shmid = libc.shmget(key, size, shmflg)
+
+    if shmid < 0:
+        raise Exception("Error creating shared memory")
+
+    logger.info(f"Shared memory ID: {shmid}")
+
+    # 附加共享内存
+    shm_addr = libc.shmat(shmid, ctypes.c_void_p(0), 0)
+
+    if shm_addr == ctypes.c_void_p(-1).value:
+        raise Exception("Error attaching shared memory")
+
+    logger.info(f"Shared memory attached at address: {shm_addr}")
+
+    return shm_addr
+
+
+@lru_cache(maxsize=None)
+def attach_shm_kv_cache_ptr():
     """
-    get cpu cache pin tensor, this function is used to get the cpu cache pin tensor.
+    Attach to the shared memory segment with the given shmid.
     """
     args = get_env_start_args()
-    assert args.enable_cpu_cache
-    # cpu_cache_page_num = calcu_cpu_cache_page_num()
-    # to do: 这里需要考虑到 cpu cache 的 page size
+    libc = ctypes.CDLL("/usr/lib/x86_64-linux-gnu/libc.so.6")
+
+    # 设置 shmget 和 shmat 函数的参数类型和返回类型
+    libc.shmget.argtypes = (ctypes.c_long, ctypes.c_size_t, ctypes.c_int)
+    libc.shmget.restype = ctypes.c_int
+    libc.shmat.argtypes = (ctypes.c_int, ctypes.c_void_p, ctypes.c_int)
+    libc.shmat.restype = ctypes.c_void_p
+
+    # 通过键获取共享内存 ID
+    key = args.cpu_kv_cache_shm_id  # 共享内存的键
+    shmid = libc.shmget(key, 0, 0)
+
+    if shmid < 0:
+        raise Exception("Error getting shared memory")
+
+    logger.info(f"Shared memory ID: {shmid}")
+
+    # 附加共享内存
+    shm_addr = libc.shmat(shmid, ctypes.c_void_p(0), 0)
+
+    if shm_addr == ctypes.c_void_p(-1).value:
+        raise Exception("Error attaching shared memory")
+
+    logger.info(f"Shared memory attached at address: {shm_addr}")
+    return shm_addr
+
+
+@dataclasses.dataclass
+class CpuKVCacheMeta:
+    page_num: int
+    layer_num: int
+    num_heads: int
+    head_dim: int
+    item_size: int
+
+    def calcu_size(self):
+        return self.page_num * self.layer_num * self.num_heads * self.head_dim * self.item_size
