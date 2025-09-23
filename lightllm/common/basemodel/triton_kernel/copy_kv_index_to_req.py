7ccf918fc25ba835e00aea5df92c5500b9419d4e
shihaobai <1798930569@qq.com>
2025-09-23 14:31:44 +0800

    backup the copy_kv_index

--- lightllm/common/basemodel/triton_kernel/copy_kv_index_to_req.py
+++ lightllm/common/basemodel/triton_kernel/copy_kv_index_to_req.py
@@ -18,9 +18,7 @@ def _fwd_kernel_copy_kv_index_to_req(
 
 
 @torch.no_grad()
-def copy_kv_index_to_req(req_to_token_indexs, b_req_idx, b_seq_len, memindex, skip_copy: bool = False):
-    if skip_copy:
-        return
+def copy_kv_index_to_req(req_to_token_indexs, b_req_idx, b_seq_len, memindex):
     seq_len = b_seq_len.shape[0]
     assert b_seq_len.shape[0] == memindex.shape[0] and b_req_idx.shape[0] == b_seq_len.shape[0]
     grid = (seq_len,)
