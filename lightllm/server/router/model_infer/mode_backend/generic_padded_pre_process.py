1554a7e9c027f4242961bcce60551f5c1a59d3ab
wangzaijun <wangzaijun@sensetime.com>
2025-10-09 06:30:23 +0000

    fix padded pre

--- lightllm/server/router/model_infer/mode_backend/generic_padded_pre_process.py
+++ lightllm/server/router/model_infer/mode_backend/generic_padded_pre_process.py
@@ -26,15 +26,12 @@ def padded_prepare_prefill_inputs(
 
     run_reqs = []
     total_token_num = 0
-    max_len_in_batch = 0
-    max_q_seq_len = 0
-    max_kv_seq_len = 0
-    max_cache_len = 0
     prefix_total_token_num = 0
     padded_req_num = dest_batch_size - len(req_objs)
     input_ids = []
     b_req_idx = []
     b_seq_len = []
+    b_q_seq_len = []
     batch_multimodal_params = []
     b_ready_cache_len = []
     b_mtp_index = []
@@ -53,10 +50,9 @@ def padded_prepare_prefill_inputs(
         input_id = input_token_ids[req.cur_kv_len :]
 
         b_seq_len.append(seq_len)
+        b_q_seq_len.append(input_token_len)
         input_ids.append(input_id)
         total_token_num += seq_len
-        max_len_in_batch = max(max_len_in_batch, input_token_len)
-        max_q_seq_len = max(max_q_seq_len, input_token_len)
         prefix_total_token_num += req.cur_kv_len
         b_ready_cache_len.append(req.cur_kv_len)
         b_mtp_index.append(0)
@@ -66,16 +62,17 @@ def padded_prepare_prefill_inputs(
         input_ids.append([1])
         b_req_idx.append(g_infer_context.req_manager.HOLD_REQUEST_ID)
         b_seq_len.append(1)
+        b_q_seq_len.append(1)
         b_mtp_index.append(0)
         b_prefill_has_output.append(False)
         b_ready_cache_len.append(0)
         total_token_num += 1
-        max_len_in_batch = max(max_len_in_batch, 1)
-        max_q_seq_len = max(max_q_seq_len, 1)
         prefix_total_token_num += 0
 
     max_kv_seq_len = max(b_seq_len)
     max_cache_len = max(b_ready_cache_len)
+    max_q_seq_len = max(b_q_seq_len)
+    max_len_in_batch = max(b_q_seq_len)
 
     input_ids = np.concatenate(input_ids, dtype=np.int64)
     input_ids = torch.tensor(input_ids, dtype=torch.int64, device="cpu")
@@ -125,37 +122,9 @@ def padded_prepare_prefill_inputs(
 def padded_prepare_decode_inputs(
     req_objs: List[InferReq], dest_batch_size: Optional[int] = None
 ) -> Tuple[ModelInput, List[InferReq], int]:
-    run_reqs = []
-    total_token_num = 0
-    max_len_in_batch = 0
-    b_req_idx = []
-    b_mtp_index = []
-    b_seq_len = []
-    max_q_seq_len = 0
-    max_kv_seq_len = 0
-    mtp_step = get_env_start_args().mtp_step
-    for req in req_objs:
-        run_reqs.append(req)
-        b_req_idx.append(req.req_idx)
-        seq_len = req.get_cur_total_len()
-        assert req.cur_kv_len == seq_len - 1
-        b_seq_len.append(seq_len)
-        total_token_num += seq_len
-        max_len_in_batch = max(max_len_in_batch, seq_len)
-        max_q_seq_len = max(max_q_seq_len, req.mtp_step + 1)
-        b_mtp_index.append(0)
-        # process the draft tokens.
-        for step in range(req.mtp_step):
-            run_reqs.append(req)
-            b_req_idx.append(req.req_idx)
-            seq_len += 1
-            b_seq_len.append(seq_len)
-            total_token_num += seq_len
-            max_len_in_batch = max(max_len_in_batch, seq_len)
-            b_mtp_index.append(step + 1)
 
     if dest_batch_size is None:
-        if len(run_reqs) == 0:
+        if len(req_objs) == 0:
             dest_batch_size = 1
         else:
             dest_batch_size = len(req_objs)
@@ -164,31 +133,58 @@ def padded_prepare_decode_inputs(
 
     padded_req_num = dest_batch_size - len(req_objs)
 
+    run_reqs = []
+    total_token_num = 0
+    b_req_idx = []
+    b_mtp_index = []
+    b_seq_len = []
+    b_q_seq_len = []
+    args_mtp_step = get_env_start_args().mtp_step
+    for req in req_objs:
+        run_reqs.append(req)
+        b_req_idx.append(req.req_idx)
+        seq_len = req.get_cur_total_len()
+        assert req.cur_kv_len == seq_len - 1
+        b_seq_len.append(seq_len)
+        total_token_num += seq_len
+        b_mtp_index.append(0)
+        # process the draft tokens.
+        for step in range(req.mtp_step):
+            run_reqs.append(req)
+            seq_len += 1
+            total_token_num += seq_len
+            b_req_idx.append(req.req_idx)
+            b_seq_len.append(seq_len)
+            b_mtp_index.append(step + 1)
+
+        b_q_seq_len.append(req.mtp_step + 1)
+
     # padding fake req for decode
     for _ in range(padded_req_num):
         seq_len = 2
+        total_token_num += seq_len
         b_req_idx.append(g_infer_context.req_manager.HOLD_REQUEST_ID)
         b_seq_len.append(seq_len)
         b_mtp_index.append(0)
-        total_token_num += seq_len
-        max_len_in_batch = max(max_len_in_batch, seq_len)
-        max_q_seq_len = max(max_q_seq_len, 1)
-        for step in range(mtp_step):
+        for step in range(args_mtp_step):
             seq_len += 1
+            total_token_num += seq_len
             b_seq_len.append(seq_len)
             b_req_idx.append(g_infer_context.req_manager.HOLD_REQUEST_ID)
-            total_token_num += seq_len
-            max_len_in_batch = max(max_len_in_batch, seq_len)
-            b_mtp_index.append(_ + 1)
+            b_mtp_index.append(step + 1)
+
+        b_q_seq_len.append(1 + args_mtp_step)
 
     max_kv_seq_len = max(b_seq_len)
+    max_q_seq_len = max(b_q_seq_len)
+    max_len_in_batch = max(b_seq_len)
 
     b_req_idx = torch.tensor(b_req_idx, dtype=torch.int32, device="cpu")
     b_seq_len = torch.tensor(b_seq_len, dtype=torch.int32, device="cpu")
     b_mtp_index = torch.tensor(b_mtp_index, dtype=torch.int32, device="cpu")
 
     # dynamic prompt cache 准备 token
-    padded_mem_indexes_num = padded_req_num * (mtp_step + 1)
+    padded_mem_indexes_num = padded_req_num * (args_mtp_step + 1)
     g_infer_state_lock.acquire()
     if g_infer_context.radix_cache is not None:
         g_infer_context.radix_cache.free_radix_cache_to_get_enough_token(b_seq_len.shape[0] - padded_mem_indexes_num)
