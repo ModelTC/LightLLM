6ffd8a87825879ee30243a390e230dcb726f74d1
sufubao <sufubao@sensetime.com>
2025-11-07 09:16:40 +0000

    fix

--- lightllm/models/deepseek3_2/layer_infer/transformer_layer_infer.py
+++ lightllm/models/deepseek3_2/layer_infer/transformer_layer_infer.py
@@ -1,5 +1,6 @@
 from functools import partial
 from typing import override
+from venv import logger
 
 import torch
 from sgl_kernel.flash_mla import flash_mla_sparse_fwd
@@ -24,6 +25,7 @@ class Deepseek3_2TransformerLayerInfer(Deepseek2TransformerLayerInfer):
             network_config=self.network_config_,
             mode=mode
         )
+        self.topk_indices = None
         return
 
     @override
@@ -35,20 +37,15 @@ class Deepseek3_2TransformerLayerInfer(Deepseek2TransformerLayerInfer):
     ) -> torch.Tensor:
         input = input.view(-1, self.embed_dim_)
 
-        if self.q_lora_rank is None:
-            q = layer_weight.q_weight_.mm(input)
-            cache_kv = layer_weight.kv_a_proj_with_mqa_.mm(input).view(-1, 1, self.kv_lora_rank + self.qk_rope_head_dim)
-        else:
-            q, cache_kv = layer_weight.qkv_a_proj_with_mqa_.mm(input).split(
-                [self.q_lora_rank, self.kv_lora_rank + self.qk_rope_head_dim], dim=-1
-            )
-            q = rmsnorm_forward(q, weight=layer_weight.q_a_layernorm_.weight, eps=self.eps_)
+        q, cache_kv = layer_weight.qkv_a_proj_with_mqa_.mm(input).split(
+            [self.q_lora_rank, self.kv_lora_rank + self.qk_rope_head_dim], dim=-1
+        )
+        q = rmsnorm_forward(q, weight=layer_weight.q_a_layernorm_.weight, eps=self.eps_)
 
-            self.indexer.hidden_states = input
-            self.indexer.q_lora = q
+        self.topk_indices = self.indexer.get_indices(input, q, infer_state, layer_weight.indexer_layer_weight)
 
-            q = layer_weight.q_b_proj_.mm(q)
-            cache_kv = cache_kv.view(-1, 1, self.kv_lora_rank + self.qk_rope_head_dim)
+        q = layer_weight.q_b_proj_.mm(q)
+        cache_kv = cache_kv.view(-1, 1, self.kv_lora_rank + self.qk_rope_head_dim)
         q = q.view(-1, self.tp_q_head_num_, self.qk_nope_head_dim + self.qk_rope_head_dim)
         q_nope, q_rope = torch.split(q, [self.qk_nope_head_dim, self.qk_rope_head_dim], dim=-1)
         rmsnorm_forward(
@@ -69,11 +66,11 @@ class Deepseek3_2TransformerLayerInfer(Deepseek2TransformerLayerInfer):
     @override
     def _bind_attention(self):
         super()._bind_attention()
-        self._context_attention_kernel = partial(Deepseek3_2TransformerLayerInfer._context_attention_flashmla_kernel_with_indexer, self)
-        self._token_attention_kernel = partial(Deepseek3_2TransformerLayerInfer._token_attention_flashmla_kernel_with_indexer, self)
+        self._context_attention_kernel = partial(Deepseek3_2TransformerLayerInfer._nsa_context_attention_kernel, self)
+        self._token_attention_kernel = partial(Deepseek3_2TransformerLayerInfer._nsa_token_attention_kernel, self)
         pass
 
-    def _context_attention_flashmla_kernel_with_indexer(
+    def _nsa_context_attention_kernel(
         self,
         q: torch.Tensor,
         kv,
@@ -85,21 +82,17 @@ class Deepseek3_2TransformerLayerInfer(Deepseek2TransformerLayerInfer):
         q_nope, q_rope = torch.split(q, [self.qk_nope_head_dim, self.qk_rope_head_dim], dim=-1)
         q_nope = layer_weight.k_b_proj_.bmm(q_nope.transpose(0, 1)).transpose(0, 1)
         q_all = torch.cat([q_nope, q_rope], dim=-1)
-        topk_indices = self.indexer.get_indices(
-            infer_state,
-            layer_weight.indexer_layer_weight,
-        ).unsqueeze(1)
 
         mla_out, _, _ = flash_mla_sparse_fwd(
             q=q_all,
             kv=infer_state.mem_manager.kv_buffer[self.layer_num_],
-            indices=topk_indices,
+            indices=self.topk_indices,
             sm_scale=self.softmax_scale,
             d_v=self.kv_lora_rank,
         )
         return mla_out
 
-    def _token_attention_flashmla_kernel_with_indexer(
+    def _nsa_token_attention_kernel(
         self, q, infer_state: Deepseek3_2FlashAttentionStateInfo, layer_weight: Deepseek3_2TransformerLayerWeight, out=None
     ):
         q_nope, q_rope = q[:, :, : -self.qk_rope_head_dim], q[:, :, -self.qk_rope_head_dim :]
@@ -107,24 +100,23 @@ class Deepseek3_2TransformerLayerInfer(Deepseek2TransformerLayerInfer):
         kv = infer_state.mem_manager.kv_buffer[self.layer_num_]
         k_rope = kv[:, :, -self.qk_rope_head_dim :].reshape(-1, 1, 1, self.qk_rope_head_dim)
         kv_nope = kv[:, :, : -self.qk_rope_head_dim].reshape(-1, 1, 1, self.kv_lora_rank)
-        topk_indices = self.indexer.get_indices(
-            infer_state,
-            layer_weight.indexer_layer_weight,
-        )
-        o = flash_attn_with_kvcache(
+        k_descale, v_descale = None, None
+        o_tensor = flash_attn_with_kvcache(
             q=q_rope,
             k_cache=k_rope,
             v_cache=kv_nope,
             qv=q_nope,
-            page_table=topk_indices,
+            page_table=self.topk_indices,
             cache_seqlens=infer_state.b_att_seq_len,
             cu_seqlens_q=infer_state.cu_seqlens_q,
             cu_seqlens_k_new=infer_state.cu_seqlens_k,
             max_seqlen_q=infer_state.max_q_seq_len,
             softmax_scale=self.softmax_scale,
             causal=True,
+            window_size=(-1, -1),
             softcap=0.0,
+            k_descale=k_descale,
+            v_descale=v_descale,
             return_softmax_lse=False,
-            num_splits=0, # TODO enable_deterministic_inference
         )
-        return o
+        return o_tensor
