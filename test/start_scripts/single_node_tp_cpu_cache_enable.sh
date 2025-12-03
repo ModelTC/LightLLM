8cacdfe57c006342786fa4ddea0589f0bac39446
wangzaijun <wangzaijun@sensetime.com>
2025-12-03 08:46:47 +0000

    fix

---
+++ test/start_scripts/single_node_tp_cpu_cache_enable.sh
@@ -0,0 +1,6 @@
+# 使能 cpu cache 功能，扩大kv cache 复用的可能性。
+LOADWORKER=18 python -m lightllm.server.api_server \
+--model_dir /mtc/models/qwen3-8b --tp 2 --dp 1 --enable_cpu_cache  --cpu_cache_storage_size 66 --cpu_cache_token_page_size 128 \
+--batch_max_tokens 4096 --chunked_prefill_size 2048 \
+--max_total_token_num 10000 \
+--mode "ppl_int8kv_flashdecoding" | tee log.txt
