8317e83d9b1d9afef542af7e51d1d0f04eaac2d3
wangzaijun <wangzaijun@sensetime.com>
2025-12-19 09:07:50 +0000

    fix qwenvl

--- lightllm/models/qwen3_vl/infer_struct.py
+++ lightllm/models/qwen3_vl/infer_struct.py
@@ -5,7 +5,6 @@ class Qwen3VLInferStateInfo(Qwen2VLInferStateInfo):
     def __init__(self):
         super().__init__()
         self.input_ids = None
-        self.deepstack_features = []
         self.img_start_token_ids = None
         self.img_token_lens = None
         self.img_start_locs = None
