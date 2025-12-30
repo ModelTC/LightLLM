acdd94e73e35e6781b389e1949328a5b8ed84c83
wangzaijun <wangzaijun@sensetime.com>
2025-12-30 08:20:36 +0000

    fix mistral mtp weight load

--- lightllm/models/mistral_mtp/layer_weights/transformer_layer_weight.py
+++ lightllm/models/mistral_mtp/layer_weights/transformer_layer_weight.py
@@ -5,6 +5,13 @@ from lightllm.common.basemodel.layer_weights.meta_weights import NormWeight
 class MistralMTPTransformerLayerWeight(LlamaTransformerLayerWeight):
     def __init__(self, layer_num, data_type, network_config, mode=[], quant_cfg=None):
         super().__init__(layer_num, data_type, network_config, mode, quant_cfg)
+
+        self._gate_weight_name = f"mtp.layers.{self.layer_num_}.mlp.gate_proj.weight"
+        self._up_weight_name = f"mtp.layers.{self.layer_num_}.mlp.up_proj.weight"
+        self._down_weight_name = f"mtp.layers.{self.layer_num_}.mlp.down_proj.weight"
+
+        self._ffn_norm_weight_name = f"mtp.layers.{self.layer_num_}.post_attention_layernorm.weight"
+        self._ffn_norm_bias_name = None
         return
 
     def _init_weight(self):
