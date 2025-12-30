7955d7714acbd3056e93556f5e8650ea70ee1c7a
wangzaijun <wangzaijun@sensetime.com>
2025-12-30 06:40:57 +0000

    fix mtp mistral model

--- lightllm/models/mistral_mtp/model.py
+++ lightllm/models/mistral_mtp/model.py
@@ -5,7 +5,6 @@ from lightllm.models.mistral_mtp.layer_infer.pre_layer_infer import MistralMTPPr
 from lightllm.models.mistral_mtp.layer_infer.transformer_layer_infer import MistralMTPTransformerLayerInfer
 from lightllm.models.mistral_mtp.layer_weights.transformer_layer_weight import MistralMTPTransformerLayerWeight
 from lightllm.common.basemodel import TpPartBaseModel
-from .layer_weights.hf_load_utils import load_hf_weights
 
 
 class MistralMTPModel(MistralTpPartModel):
@@ -45,34 +44,14 @@ class MistralMTPModel(MistralTpPartModel):
         return
 
     def _init_weights(self):
-        self.pre_post_weight = self.pre_and_post_weight_class(
-            self.data_type, network_config=self.config, mode=self.mode
-        )
-        num_layer = 1
-        self.trans_layers_weight = [
-            self.transformer_weight_class(
-                i,
-                self.data_type,
-                network_config=self.config,
-                mode=self.mode,
-                quant_cfg=self.quant_cfg,
-            )
-            for i in range(num_layer)
-        ]
-        load_hf_weights(
-            self.data_type,
-            weight_dir=self.weight_dir_,
-            pre_post_layer=self.pre_post_weight,
-            transformer_layer_list=self.trans_layers_weight,
-            weight_dict=self.weight_dict,
-        )
-        self.pre_post_weight.verify_load()
-        [weight.verify_load() for weight in self.trans_layers_weight]
+        self.config["n_layer"] = 1
+        super()._init_weights()
         self.pre_post_weight.wte_weight_ = self.main_model.pre_post_weight.wte_weight_
         self.pre_post_weight.lm_head_weight_ = self.main_model.pre_post_weight.lm_head_weight_
         self.pre_post_weight.final_norm_weight_ = self.main_model.pre_post_weight.final_norm_weight_
         return
 
     def _init_infer_layer(self):
+        self.config["n_layer"] = 1
         super()._init_infer_layer()
         return
