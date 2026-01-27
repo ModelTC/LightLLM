a2dd0729f5a1499d2a2a43cd78eb466f97a6eb90
wangzaijun <wangzaijun@sensetime.com>
2026-01-27 10:56:26 +0000

    fix redundancy

--- lightllm/common/basemodel/layer_weights/meta_weights/fused_moe/ep_redundancy.py
+++ lightllm/common/basemodel/layer_weights/meta_weights/fused_moe/ep_redundancy.py
@@ -25,12 +25,13 @@ class FusedMoeWeightEPAutoRedundancy:
     ):
         expert_counter = self._ep_w.routed_expert_counter_tensor.detach().cpu().numpy()
         logger.info(
-            f"layer_index {self._ep_w.layer_num} global_rank {self._ep_w.global_rank_} expert_counter: {expert_counter}"
+            f"layer_index {self._ep_w.layer_num_} global_rank {self._ep_w.global_rank_}"
+            f" expert_counter: {expert_counter}"
         )
         self._ep_w.routed_expert_counter_tensor.fill_(0)
-
-        start_expert_id = self._ep_w.ep_n_routed_experts * self._ep_w.global_rank_
-        no_redundancy_expert_ids = list(range(start_expert_id, start_expert_id + self._ep_w.ep_n_routed_experts))
+        ep_n_routed_experts = self._ep_w.n_routed_experts // self._ep_w.global_world_size
+        start_expert_id = ep_n_routed_experts * self._ep_w.global_rank_
+        no_redundancy_expert_ids = list(range(start_expert_id, start_expert_id + ep_n_routed_experts))
 
         # 统计 0 rank 上的全局 topk 冗余信息，帮助导出一份全局可用的静态使用的冗余专家静态配置。
         if self._ep_w.global_rank_ == 0:
@@ -44,7 +45,7 @@ class FusedMoeWeightEPAutoRedundancy:
 
         self.redundancy_expert_ids = list(np.argsort(expert_counter)[-self.redundancy_expert_num :])
         logger.info(
-            f"layer_index {self._ep_w.layer_num} global_rank {self._ep_w.global_rank_}"
+            f"layer_index {self._ep_w.layer_num_} global_rank {self._ep_w.global_rank_}"
             f" new select redundancy_expert_ids : {self.redundancy_expert_ids}"
         )
 
@@ -55,7 +56,7 @@ class FusedMoeWeightEPAutoRedundancy:
         self.experts_gate_proj_scales = [None] * self.redundancy_expert_num
         self.w2_list = [None] * self.redundancy_expert_num
         self.w2_scale_list = [None] * self.redundancy_expert_num
-        self.w1 = [None, None]  # weight, weight_scale
+        self.w13 = [None, None]  # weight, weight_scale
         self.w2 = [None, None]  # weight, weight_scale
         return topk_redundancy_expert_ids
 
@@ -73,13 +74,12 @@ class FusedMoeWeightEPAutoRedundancy:
             if w2_weight in weights:
                 self.w2_list[i] = weights[w2_weight]
 
-        if self._ep_w.quantized_weight:
-            self._load_weight_scale(weights)
+        self._load_weight_scale(weights)
         self._fuse()
 
     def _fuse(self):
-        if self._ep_w.quantized_weight:
-            self._fuse_weight_scale()
+        self._fuse_weight_scale()
+
         with self._ep_w.lock:
             if (
                 hasattr(self, "experts_up_projs")
@@ -93,23 +93,38 @@ class FusedMoeWeightEPAutoRedundancy:
                 dtype = self.experts_gate_projs[0].dtype
                 total_expert_num = self.redundancy_expert_num
 
-                w1 = torch.empty((total_expert_num, gate_out_dim + up_out_dim, gate_in_dim), dtype=dtype, device="cpu")
+                w13 = torch.empty((total_expert_num, gate_out_dim + up_out_dim, gate_in_dim), dtype=dtype, device="cpu")
 
                 for i_experts in range(self.redundancy_expert_num):
-                    w1[i_experts, 0:gate_out_dim:, :] = self.experts_gate_projs[i_experts]
-                    w1[i_experts, gate_out_dim:, :] = self.experts_up_projs[i_experts]
+                    w13[i_experts, 0:gate_out_dim:, :] = self.experts_gate_projs[i_experts]
+                    w13[i_experts, gate_out_dim:, :] = self.experts_up_projs[i_experts]
 
                 inter_shape, hidden_size = self.w2_list[0].shape[0], self.w2_list[0].shape[1]
                 w2 = torch._utils._flatten_dense_tensors(self.w2_list).view(len(self.w2_list), inter_shape, hidden_size)
-                if not self._ep_w.quantized_weight and self._ep_w.quant_method is not None:
-                    qw1_pack = self._ep_w.quant_method.quantize(w1)
-                    qw2_pack = self._ep_w.quant_method.quantize(w2)
-                    self.w1[0] = qw1_pack.weight
-                    self.w1[1] = qw1_pack.weight_scale
-                    self.w2[0] = qw2_pack.weight
-                    self.w2[1] = qw2_pack.weight_scale
+                if self._ep_w.quant_method._check_weight_need_quanted(weight=w13):
+                    w13_pack, _ = self._ep_w.quant_method.create_moe_weight(
+                        out_dims=[gate_out_dim + up_out_dim],
+                        in_dim=1,
+                        dtype=self._ep_w.data_type_,
+                        device_id=self._ep_w.device_id_,
+                        num_experts=self.redundancy_expert_num,
+                    )
+                    self._ep_w.quant_method.quantize(w13, w13_pack)
+                    w2_pack, _ = self._ep_w.quant_method.create_moe_weight(
+                        out_dims=[inter_shape],
+                        in_dim=hidden_size,
+                        dtype=self._ep_w.data_type_,
+                        device_id=self._ep_w.device_id_,
+                        num_experts=self.redundancy_expert_num,
+                    )
+                    self._ep_w.quant_method.quantize(w2, w2_pack)
+
+                    self.w13[0] = w13_pack.weight
+                    self.w13[1] = w13_pack.weight_scale
+                    self.w2[0] = w2_pack.weight
+                    self.w2[1] = w2_pack.weight_scale
                 else:
-                    self.w1[0] = w1
+                    self.w13[0] = w13
                     self.w2[0] = w2
                 delattr(self, "w2_list")
                 delattr(self, "experts_up_projs")
@@ -128,18 +143,18 @@ class FusedMoeWeightEPAutoRedundancy:
                 assert gate_in_dim == up_in_dim
                 dtype = self.experts_gate_proj_scales[0].dtype
                 total_expert_num = self.redundancy_expert_num
-                w1_scale = torch.empty(
+                w13_scale = torch.empty(
                     (total_expert_num, gate_out_dim + up_out_dim, gate_in_dim), dtype=dtype, device="cpu"
                 )
                 for i_experts in range(self.redundancy_expert_num):
-                    w1_scale[i_experts, 0:gate_out_dim:, :] = self.experts_gate_proj_scales[i_experts]
-                    w1_scale[i_experts, gate_out_dim:, :] = self.experts_up_proj_scales[i_experts]
+                    w13_scale[i_experts, 0:gate_out_dim:, :] = self.experts_gate_proj_scales[i_experts]
+                    w13_scale[i_experts, gate_out_dim:, :] = self.experts_up_proj_scales[i_experts]
 
                 inter_shape, hidden_size = self.w2_scale_list[0].shape[0], self.w2_scale_list[0].shape[1]
                 w2_scale = torch._utils._flatten_dense_tensors(self.w2_scale_list).view(
                     len(self.w2_scale_list), inter_shape, hidden_size
                 )
-                self.w1[1] = w1_scale
+                self.w13[1] = w13_scale
                 self.w2[1] = w2_scale
                 delattr(self, "w2_scale_list")
                 delattr(self, "experts_up_proj_scales")
@@ -149,15 +164,10 @@ class FusedMoeWeightEPAutoRedundancy:
         # 加载冗余专家的scale参数
         for i, redundant_expert_id in enumerate(self.redundancy_expert_ids):
             i_experts = redundant_expert_id
-            w1_scale = (
-                f"{self._ep_w.weight_prefix}.{i_experts}.{self._ep_w.w1_weight_name}.{self._ep_w.weight_scale_suffix}"
-            )
-            w2_scale = (
-                f"{self._ep_w.weight_prefix}.{i_experts}.{self._ep_w.w2_weight_name}.{self._ep_w.weight_scale_suffix}"
-            )
-            w3_scale = (
-                f"{self._ep_w.weight_prefix}.{i_experts}.{self._ep_w.w3_weight_name}.{self._ep_w.weight_scale_suffix}"
-            )
+            weight_scale_suffix = self._ep_w.quant_method.weight_scale_suffix
+            w1_scale = f"{self._ep_w.weight_prefix}.{i_experts}.{self._ep_w.w1_weight_name}.{weight_scale_suffix}"
+            w2_scale = f"{self._ep_w.weight_prefix}.{i_experts}.{self._ep_w.w2_weight_name}.{weight_scale_suffix}"
+            w3_scale = f"{self._ep_w.weight_prefix}.{i_experts}.{self._ep_w.w3_weight_name}.{weight_scale_suffix}"
             if w1_scale in weights:
                 self.experts_gate_proj_scales[i] = weights[w1_scale]
             if w3_scale in weights:
@@ -166,14 +176,14 @@ class FusedMoeWeightEPAutoRedundancy:
                 self.w2_scale_list[i] = weights[w2_scale]
 
     def commit(self):
-        for index, dest_tensor in enumerate(self._ep_w.w1):
+        for index, dest_tensor in enumerate([self._ep_w.w13.weight, self._ep_w.w13.weight_scale]):
             if dest_tensor is not None:
                 assert isinstance(
                     dest_tensor, torch.Tensor
                 ), f"dest_tensor should be a torch.Tensor, but got {type(dest_tensor)}"
-                dest_tensor[-self.redundancy_expert_num :, :, :] = self.w1[index][:, :, :]
+                dest_tensor[-self.redundancy_expert_num :, :, :] = self.w13[index][:, :, :]
 
-        for index, dest_tensor in enumerate(self._ep_w.w2):
+        for index, dest_tensor in enumerate([self._ep_w.w2.weight, self._ep_w.w2.weight_scale]):
             if dest_tensor is not None:
                 assert isinstance(
                     dest_tensor, torch.Tensor
