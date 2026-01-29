3c1313c36c521a5bbcdb0408451ab7f179421386
WANDY666 <1060304770@qq.com>
2026-01-29 06:49:31 +0000

    merge qkv

--- lightllm/models/qwen3_moe/layer_infer/transformer_layer_infer.py
+++ lightllm/models/qwen3_moe/layer_infer/transformer_layer_infer.py
@@ -60,8 +60,10 @@ class Qwen3MOETransformerLayerInfer(LlamaTransformerLayerInfer):
         layer_weight: Qwen3MOETransformerLayerWeight,
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         input = input.view(-1, self.embed_dim_)
-        q = layer_weight.q_proj.mm(input)
-        cache_kv = layer_weight.kv_proj.mm(input)
+        qkv = layer_weight.qkv_proj.mm(input)
+        q, cache_kv = qkv.split(
+            [self.tp_q_head_num_ * self.head_dim_, (self.tp_k_head_num_ + self.tp_v_head_num_) * self.head_dim_], dim=-1
+        )
         layer_weight.q_norm_weight_(q, eps=self.eps_)
         layer_weight.k_norm_weight_(
             cache_kv[:, : self.tp_k_head_num_ * self.head_dim_],
