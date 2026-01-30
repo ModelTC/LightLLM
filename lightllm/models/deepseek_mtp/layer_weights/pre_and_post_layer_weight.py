df9141ed44aa49c0ca806912aee6e0e236a2aa33
sufubao <sufubao@sensetime.com>
2026-01-30 06:37:50 +0000

    fix

--- lightllm/models/deepseek_mtp/layer_weights/pre_and_post_layer_weight.py
+++ lightllm/models/deepseek_mtp/layer_weights/pre_and_post_layer_weight.py
@@ -9,32 +9,32 @@ from lightllm.common.quantization import Quantcfg
 
 
 class Deepseek3MTPPreAndPostLayerWeight(PreAndPostLayerWeight):
-    def __init__(self, data_type, network_config, quant_cfg: Quantcfg, layer_idx=0):
+    def __init__(self, data_type, network_config, quant_cfg: Quantcfg):
         super().__init__(data_type, network_config)
         self.quant_cfg: Quantcfg = quant_cfg
         hidden_size = network_config["hidden_size"]
         self.eh_proj_weight_ = ROWMMWeight(
             in_dim=hidden_size * 2,
             out_dims=[hidden_size],
-            weight_names=f"model.layers.{layer_idx}.eh_proj.weight",
+            weight_names="model.layers.0.eh_proj.weight",
             data_type=self.data_type_,
-            quant_method=self.quant_cfg.get_quant_method(layer_idx, "eh_proj"),
+            quant_method=self.quant_cfg.get_quant_method(0, "eh_proj"),
             tp_rank=0,
             tp_world_size=1,
         )
         self.enorm_weight_ = RMSNormWeight(
             dim=hidden_size,
-            weight_name=f"model.layers.{layer_idx}.enorm.weight",
+            weight_name="model.layers.0.enorm.weight",
             data_type=self.data_type_,
         )
         self.hnorm_weight_ = RMSNormWeight(
             dim=hidden_size,
-            weight_name=f"model.layers.{layer_idx}.hnorm.weight",
+            weight_name="model.layers.0.hnorm.weight",
             data_type=self.data_type_,
         )
         self.final_norm_weight_ = RMSNormWeight(
             dim=hidden_size,
-            weight_name=f"model.layers.{layer_idx}.shared_head.norm.weight",
+            weight_name="model.layers.0.shared_head.norm.weight",
             data_type=self.data_type_,
         )
 
