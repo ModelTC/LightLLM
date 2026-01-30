78cc66ab78657f5345a6d8e8ba9ace2636287e98
sufubao <47234901+sufubao@users.noreply.github.com>
2026-01-30 16:09:23 +0800

    feat(model): Add GLM-4.7-Flash model support (#1188)
    Co-authored-by: shihaobai <1798930569@qq.com>
    Co-authored-by: wangzaijun <wangzaijun@sensetime.com>

--- lightllm/server/router/model_infer/mode_backend/base_backend.py
+++ lightllm/server/router/model_infer/mode_backend/base_backend.py
@@ -37,6 +37,7 @@ from lightllm.server.router.model_infer.mode_backend.overlap_events import Overl
 from lightllm.models.deepseek_mtp.model import Deepseek3MTPModel
 from lightllm.models.qwen3_moe_mtp.model import Qwen3MOEMTPModel
 from lightllm.models.mistral_mtp.model import MistralMTPModel
+from lightllm.models.glm4_moe_lite_mtp.model import Glm4MoeLiteMTPModel
 from lightllm.server.router.model_infer.mode_backend.generic_post_process import sample
 from lightllm.common.basemodel.triton_kernel.gather_token_id import scatter_token
 from lightllm.server.pd_io_struct import NIXLChunckedTransTaskRet
@@ -328,6 +329,9 @@ class ModeBackend:
             elif mtp_model_cfg["model_type"] == "mistral":
                 assert self.args.mtp_mode in ["vanilla_no_att", "eagle_no_att"]
                 self.draft_models.append(MistralMTPModel(mtp_model_kvargs))
+            elif mtp_model_cfg["model_type"] == "glm4_moe_lite":
+                assert self.args.mtp_mode in ["vanilla_with_att", "eagle_with_att"]
+                self.draft_models.append(Glm4MoeLiteMTPModel(mtp_model_kvargs))
             else:
                 assert False, f"error mtp mode {mtp_model_cfg['model_type']}"
 
