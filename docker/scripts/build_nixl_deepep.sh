da993fc4b8dc8b83e62d355bbe1f9c850d172a50
shihaobai <1798930569@qq.com>
2026-02-02 09:07:01 +0000

    fix

--- docker/scripts/build_nixl_deepep.sh
+++ docker/scripts/build_nixl_deepep.sh
@@ -5,7 +5,7 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 cd "${ROOT_DIR}"
 
 IMAGE_PREFIX="${IMAGE_PREFIX:-lightllm}"
-CUDA_VERSION="${CUDA_VERSION:-12.6.1}"
+CUDA_VERSION="${CUDA_VERSION:-12.8.0}"
 IMAGE_TAG="${IMAGE_TAG:-nixl.deepep-cuda${CUDA_VERSION}}"
 
 DOCKER_BUILDKIT=1 docker build -f docker/Dockerfile \
