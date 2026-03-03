b3ce1f0993056f7f35cb3ea0d7ced67e993dd319
wangzaijun <wangzaijun@sensetime.com>
2026-03-03 08:30:18 +0000

    fix deepseekv3.2

--- lightllm/common/kv_cache_mem_manager/deepseek3_2mem_manager.py
+++ lightllm/common/kv_cache_mem_manager/deepseek3_2mem_manager.py
@@ -1,6 +1,5 @@
 import torch
 
-from lightllm.common.kv_cache_mem_manager.mem_manager import MemoryManager
 from lightllm.common.kv_cache_mem_manager.deepseek2_mem_manager import Deepseek2MemoryManager
 
 
@@ -24,8 +23,3 @@ class Deepseek3_2MemoryManager(Deepseek2MemoryManager):
     def resize_mem(self, new_size):
         super().resize_mem(new_size)
         self.indexer_ks_buffer = IndexerKSBuffer(self.size, 1, 132, self.layer_num)
-
-
-class Deepseek3_2FP8KVMemoryManager(Deepseek3_2MemoryManager):
-    def __init__(self, size, dtype, head_num, head_dim, layer_num, always_copy=False, mem_fraction=0.9):
-        super().__init__(size, torch.uint8, head_num, head_dim + 2, layer_num, always_copy, mem_fraction)
--- lightllm/models/deepseek3_2/model.py
+++ lightllm/models/deepseek3_2/model.py
@@ -1,25 +1,10 @@
 import copy
-import json
-import logging
-import os
-
 from lightllm.models.registry import ModelRegistry
 from lightllm.models.deepseek2.model import Deepseek2TpPartModel
-from lightllm.utils.envs_utils import get_env_start_args
-
-_logger = logging.getLogger(__name__)
-
-# When ENABLE_NSA is set, use the full V32 NSA (Native Sparse Attention) pipeline
-# including the indexer, custom memory manager, and NSA-aware attention kernels.
-# When not set, fall back to the DeepSeek V3 (Deepseek2) inference path while
-# keeping V32-specific tokenizer/parser support intact.
-_ENABLE_NSA = os.environ.get("ENABLE_NSA", "0").lower() in ("1", "true")
-
-if _ENABLE_NSA:
-    from lightllm.models.deepseek3_2.layer_weights.transformer_layer_weight import Deepseek3_2TransformerLayerWeight
-    from lightllm.models.deepseek3_2.layer_infer.transformer_layer_infer import Deepseek3_2TransformerLayerInfer
-    from lightllm.models.deepseek3_2.infer_struct import Deepseek3_2InferStateInfo
-    from lightllm.models.deepseek3_2.mem_manager import Deepseek3_2MemoryManager, Deepseek3_2FP8KVMemoryManager
+from lightllm.models.deepseek3_2.layer_weights.transformer_layer_weight import Deepseek3_2TransformerLayerWeight
+from lightllm.models.deepseek3_2.layer_infer.transformer_layer_infer import Deepseek3_2TransformerLayerInfer
+from lightllm.models.deepseek3_2.infer_struct import Deepseek3_2InferStateInfo
+from lightllm.common.basemodel.attention import get_nsa_prefill_att_backend_class, get_nsa_decode_att_backend_class
 
 
 class DeepSeekV32Tokenizer:
@@ -114,49 +99,17 @@ class DeepSeekV32Tokenizer:
 
 @ModelRegistry(["deepseek_v32"])
 class Deepseek3_2TpPartModel(Deepseek2TpPartModel):
-    # When ENABLE_NSA is set, override with V32-specific NSA classes.
-    # Otherwise, inherit the V3/V2 classes from Deepseek2TpPartModel.
-    if _ENABLE_NSA:
-        transformer_weight_class = Deepseek3_2TransformerLayerWeight
-        transformer_layer_infer_class = Deepseek3_2TransformerLayerInfer
-        infer_state_class = Deepseek3_2InferStateInfo
 
-    def __init__(self, kvargs):
-        super().__init__(kvargs)
-        if _ENABLE_NSA:
-            self.index_topk = self.config["index_topk"]
-        else:
-            _logger.info("ENABLE_NSA is not set, using DeepSeek V3 inference path (no NSA indexer).")
-        return
+    # weight class
+    transformer_weight_class = Deepseek3_2TransformerLayerWeight
 
-    def _init_inferstate_cls(self):
-        if _ENABLE_NSA:
-            self.infer_state_class = Deepseek3_2InferStateInfo
-        else:
-            super()._init_inferstate_cls()
+    # infer class
+    transformer_layer_infer_class = Deepseek3_2TransformerLayerInfer
 
-    def _init_mem_manager(self):
-        if not _ENABLE_NSA:
-            # Fall back to the standard V3/V2 memory manager (no indexer buffer).
-            return super()._init_mem_manager()
+    # infer state class
+    infer_state_class = Deepseek3_2InferStateInfo
 
-        manager_class = Deepseek3_2MemoryManager
-        if get_env_start_args().llm_kv_type == "fp8kv":
-            manager_class = Deepseek3_2FP8KVMemoryManager
-
-        # mtp 模式下需要在mem manger上扩展draft model使用的layer
-        added_mtp_layer_num = 0
-        if get_env_start_args().mtp_mode == "deepseekv3_eagle":
-            added_mtp_layer_num += 1
-        elif get_env_start_args().mtp_mode == "deepseekv3_vanilla":
-            added_mtp_layer_num += get_env_start_args().mtp_step
-
-        self.mem_manager = manager_class(
-            self.max_total_token_num,
-            dtype=self.data_type,
-            head_num=1,
-            head_dim=self.config["kv_lora_rank"] + self.config["qk_rope_head_dim"],
-            layer_num=self.config["num_hidden_layers"] + added_mtp_layer_num,
-            mem_fraction=self.mem_fraction,
-        )
+    def _init_att_backend(self):
+        self.prefill_att_backend = get_nsa_prefill_att_backend_class(index=0)(model=self)
+        self.decode_att_backend = get_nsa_decode_att_backend_class(index=0)(model=self)
         return
