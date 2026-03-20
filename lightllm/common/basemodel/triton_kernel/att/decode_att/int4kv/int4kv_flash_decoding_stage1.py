0cc396a0f891fc72a881bf2f30c791b9a8a13d9f
wangzaijun <wzjhelloworld@qq.com>
2026-03-20 03:26:43 +0000

    fix int4kv

--- lightllm/common/basemodel/triton_kernel/att/decode_att/int4kv/int4kv_flash_decoding_stage1.py
+++ lightllm/common/basemodel/triton_kernel/att/decode_att/int4kv/int4kv_flash_decoding_stage1.py
@@ -54,77 +54,81 @@ def _fwd_kernel_flash_decode_stage1(
     stride_mid_o_es,
     gqa_group_size,
     quant_group_size,
+    BLOCK_HEAD: tl.constexpr,
     BLOCK_SEQ: tl.constexpr,
     BLOCK_DMODEL: tl.constexpr,
     BLOCK_N: tl.constexpr,
 ):
     cur_batch = tl.program_id(0)
-    cur_head = tl.program_id(1)
-    seq_start_block = tl.program_id(2)
-    cur_kv_head = cur_head // gqa_group_size
+    cur_kv_head = tl.program_id(1)
+    block_index = tl.program_id(2)
+    grid_block_num = tl.num_programs(2)
 
-    offs_d = tl.arange(0, BLOCK_DMODEL)
     cur_batch_seq_len = tl.load(B_Seqlen + cur_batch)
+    req_total_block_num = tl.cdiv(cur_batch_seq_len, BLOCK_SEQ)
+    if block_index >= req_total_block_num:
+        return
+
     cur_batch_req_idx = tl.load(B_req_idx + cur_batch)
-    cur_batch_start_index = seq_start_block * BLOCK_SEQ
-    cur_batch_end_index = tl.minimum(cur_batch_seq_len, cur_batch_start_index + BLOCK_SEQ)
+    off_head = cur_kv_head * gqa_group_size + tl.arange(0, BLOCK_HEAD)
+    off_head = tl.where(tl.arange(0, BLOCK_HEAD) < gqa_group_size, off_head, cur_kv_head * gqa_group_size)
+    offs_d = tl.arange(0, BLOCK_DMODEL)
 
-    off_q = cur_batch * stride_qbs + cur_head * stride_qh + offs_d
-
-    block_n_size = (
-        tl.where(
-            cur_batch_end_index - cur_batch_start_index <= 0,
-            0,
-            cur_batch_end_index - cur_batch_start_index + BLOCK_N - 1,
-        )
-        // BLOCK_N
-    )
-
-    offs_n = cur_batch_start_index + tl.arange(0, BLOCK_N)
-
+    tl.device_assert(stride_qd == 1)
+    off_q = cur_batch * stride_qbs + off_head[:, None] * stride_qh + offs_d[None, :]
     q = tl.load(Q + off_q)
 
-    sum_exp = 0.0
-    max_logic = -float("inf")
-    acc = tl.zeros([BLOCK_DMODEL], dtype=tl.float32)
+    sum_exp = tl.zeros([BLOCK_HEAD], dtype=tl.float32)
+    max_logic = tl.zeros([BLOCK_HEAD], dtype=tl.float32) - float("inf")
+    acc = tl.zeros([BLOCK_HEAD, BLOCK_DMODEL], dtype=tl.float32)
 
-    for start_n in range(0, block_n_size, 1):
-        offs_n_new = start_n * BLOCK_N + offs_n
-        k_loc = tl.load(
-            Req_to_tokens + stride_req_to_tokens_b * cur_batch_req_idx + offs_n_new,
-            mask=offs_n_new < cur_batch_end_index,
-            other=0,
+    for iter_block_index in range(block_index, req_total_block_num, grid_block_num):
+        cur_batch_start_index = iter_block_index * BLOCK_SEQ
+        cur_batch_end_index = tl.minimum(cur_batch_seq_len, cur_batch_start_index + BLOCK_SEQ)
+        block_n_size = tl.cdiv(cur_batch_end_index - cur_batch_start_index, BLOCK_N)
+
+        offs_n = cur_batch_start_index + tl.arange(0, BLOCK_N)
+
+        for start_n in range(0, block_n_size, 1):
+            offs_n_new = start_n * BLOCK_N + offs_n
+            k_loc = tl.load(
+                Req_to_tokens + stride_req_to_tokens_b * cur_batch_req_idx + offs_n_new,
+                mask=offs_n_new < cur_batch_end_index,
+                other=0,
+            )
+            k_loc = k_loc.to(tl.int64)
+            off_k = k_loc[:, None] * stride_kbs + cur_kv_head * stride_kh + offs_d[None, :] // 2
+            off_k_scale = off_k // (quant_group_size // 2)
+            k_int8 = tl.load(K + off_k, mask=offs_n_new[:, None] < cur_batch_end_index, other=0)
+            k_scale = tl.load(K_scale + off_k_scale, mask=offs_n_new[:, None] < cur_batch_end_index, other=0.0)
+            k = int4_to_float(k_int8, k_scale, offs_d)
+
+            att_value = tl.dot(q, k.T)
+            att_value *= sm_scale
+            att_value = tl.where((offs_n_new[None, :] < cur_batch_end_index), att_value, float("-inf"))
+            v_int8 = tl.load(V + off_k, mask=offs_n_new[:, None] < cur_batch_end_index, other=0)
+            v_scale = tl.load(V_scale + off_k_scale, mask=offs_n_new[:, None] < cur_batch_end_index, other=0.0)
+            v = int4_to_float(v_int8, v_scale, offs_d)
+
+            cur_max_logic = tl.max(att_value, axis=1)
+            new_max_logic = tl.maximum(cur_max_logic, max_logic)
+
+            exp_logic = tl.exp(att_value - new_max_logic[:, None])
+            logic_scale = tl.exp(max_logic - new_max_logic)
+            acc *= logic_scale[:, None]
+            acc += tl.dot(exp_logic.to(v.dtype), v)
+
+            sum_exp = sum_exp * logic_scale + tl.sum(exp_logic, axis=1)
+            max_logic = new_max_logic
+
+        off_mid_o = (
+            cur_batch * stride_mid_ob
+            + off_head[:, None] * stride_mid_oh
+            + block_index * stride_mid_os
+            + offs_d[None, :]
         )
-        k_loc = k_loc.to(tl.int64)
-        off_k = k_loc[:, None] * stride_kbs + cur_kv_head * stride_kh + offs_d[None, :] // 2
-        off_k_scale = off_k // (quant_group_size // 2)
-        k_int8 = tl.load(K + off_k, mask=offs_n_new[:, None] < cur_batch_end_index, other=0)
-        k_scale = tl.load(K_scale + off_k_scale, mask=offs_n_new[:, None] < cur_batch_end_index, other=0.0)
-        k = int4_to_float(k_int8, k_scale, offs_d)
-
-        att_value = tl.sum(q[None, :] * k, 1)
-        att_value *= sm_scale
-        att_value = tl.where((offs_n_new < cur_batch_end_index), att_value, float("-inf"))
-        v_int8 = tl.load(V + off_k, mask=offs_n_new[:, None] < cur_batch_end_index, other=0)
-        v_scale = tl.load(V_scale + off_k_scale, mask=offs_n_new[:, None] < cur_batch_end_index, other=0.0)
-        v = int4_to_float(v_int8, v_scale, offs_d)
-
-        cur_max_logic = tl.max(att_value, axis=0)
-        new_max_logic = tl.maximum(cur_max_logic, max_logic)
-
-        exp_logic = tl.exp(att_value - new_max_logic)
-        logic_scale = tl.exp(max_logic - new_max_logic)
-        acc *= logic_scale
-        acc += tl.sum(exp_logic[:, None] * v, axis=0)
-
-        sum_exp = sum_exp * logic_scale + tl.sum(exp_logic, axis=0)
-        max_logic = new_max_logic
-
-    need_store = tl.where(block_n_size == 0, 0, 1)
-    for _ in range(0, need_store, 1):
-        off_mid_o = cur_batch * stride_mid_ob + cur_head * stride_mid_oh + seq_start_block * stride_mid_os + offs_d
-        off_mid_o_logexpsum = cur_batch * stride_mid_o_eb + cur_head * stride_mid_o_eh + seq_start_block
-        tl.store(Mid_O + off_mid_o, acc / sum_exp)
+        off_mid_o_logexpsum = cur_batch * stride_mid_o_eb + off_head * stride_mid_o_eh + block_index
+        tl.store(Mid_O + off_mid_o, acc / sum_exp[:, None])
         tl.store(Mid_O_LogExpSum + off_mid_o_logexpsum, max_logic + tl.log(sum_exp))
     return
 
@@ -139,7 +143,7 @@ def int4kv_flash_decode_stage1(
     Req_to_tokens,
     B_req_idx,
     B_Seqlen,
-    max_len_in_batch,
+    max_kv_seq_len,
     mid_out,
     mid_out_logsumexp,
     block_seq,
@@ -152,8 +156,9 @@ def int4kv_flash_decode_stage1(
     assert Lq == Lk
     assert Lk in {16, 32, 64, 128}
     sm_scale = 1.0 / (Lk ** 0.5)
-    batch, head_num = B_req_idx.shape[0], q.shape[1]
-    grid = (batch, head_num, triton.cdiv(max_len_in_batch, BLOCK_SEQ))
+    batch, kv_head_num = B_req_idx.shape[0], k.shape[1]
+    grid_block_num = mid_out.shape[2]
+    grid = (batch, kv_head_num, grid_block_num)
     gqa_group_size = q.shape[1] // k.shape[1]
     quant_group_size = Lk // k_scale.shape[-1]
     assert triton.next_power_of_2(quant_group_size) == quant_group_size
@@ -189,8 +194,9 @@ def int4kv_flash_decode_stage1(
         mid_out_logsumexp.stride(0),
         mid_out_logsumexp.stride(1),
         mid_out_logsumexp.stride(2),
-        gqa_group_size,
-        quant_group_size,
+        gqa_group_size=gqa_group_size,
+        quant_group_size=quant_group_size,
+        BLOCK_HEAD=triton.next_power_of_2(gqa_group_size),
         BLOCK_SEQ=BLOCK_SEQ,
         BLOCK_DMODEL=Lk,
         BLOCK_N=BLOCK_N,
