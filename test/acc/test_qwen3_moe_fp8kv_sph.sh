11769935e56e7ebe07383f17902d79a9379db5c9
wangzaijun <wzjhelloworld@qq.com>
2026-03-21 07:23:33 +0000

    fix

--- test/acc/test_qwen3_moe_fp8kv_sph.sh
+++
@@ -1,7 +0,0 @@
-# 先清理掉占用显卡和端口的进程
-
-# first
-LOADWORKER=18 CUDA_VISIBLE_DEVICES=6,7 python -m lightllm.server.api_server --model_dir /mtc/models/Qwen3-30B-A3B --tp 2 --port 8089 --llm_kv_type fp8kv_sph --kv_quant_calibration_config_path /mtc/wzj/lightllm_dev/LightLLM/test/advanced_config/fp8_calibration_per_head/test_kv_cache_calib_per_head_qwen3_30b.json
-
-# second
-HF_ALLOW_CODE_EVAL=1 HF_DATASETS_OFFLINE=0 lm_eval --model local-completions --model_args '{"model":"Qwen/Qwen3-30B-A3B-Instruct-2507", "base_url":"http://localhost:8089/v1/completions", "max_length": 16384}' --tasks gsm8k --batch_size 64 --confirm_run_unsafe_code
