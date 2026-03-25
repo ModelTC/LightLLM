77bfcbaf819075f41def3111ee9a9649ab151e55
sufubao <sufubao@sensetime.com>
2026-03-25 22:06:38 +0800

    fix

--- lightllm/models/qwen3_5/model.py
+++ lightllm/models/qwen3_5/model.py
@@ -9,8 +9,9 @@ from lightllm.models.qwen3_vl.model import QWen3VLTokenizer
 from lightllm.models.qwen3_vl.layer_infer.pre_layer_infer import (
     Qwen3VLMultimodalPreLayerInfer,
 )
-from lightllm.models.qwen3_5.layer_weights.pre_and_post_layer_weight import Qwen35PreAndPostLayerWeight
-
+from lightllm.models.qwen3_vl.layer_weights.pre_and_post_layer_weight import (
+    Qwen3VLPreAndPostLayerWeight,
+)
 from lightllm.models.qwen3_5.layer_infer.transformer_layer_infer import (
     Qwen35TransformerLayerInfer,
 )
@@ -51,7 +52,7 @@ class Qwen3_5TpPartModel(Qwen3NextTpPartModel):
     """
 
     transformer_weight_class = Qwen35TransformerLayerWeight
-    pre_and_post_weight_class = Qwen35PreAndPostLayerWeight
+    pre_and_post_weight_class = Qwen3VLPreAndPostLayerWeight
 
     pre_layer_infer_class = Qwen3VLMultimodalPreLayerInfer
     transformer_layer_infer_class = Qwen35TransformerLayerInfer
