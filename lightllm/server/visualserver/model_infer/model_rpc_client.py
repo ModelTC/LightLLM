53b9127c214963680205ae55687b75646e45d7f6
wzj <wzjhelloworld@qq.com>
2026-03-28 09:58:33 +0000

    fix

--- lightllm/server/visualserver/model_infer/model_rpc_client.py
+++ lightllm/server/visualserver/model_infer/model_rpc_client.py
@@ -4,12 +4,11 @@ import threading
 from typing import Dict, List, Tuple, Deque, Optional, Union
 from lightllm.server.multimodal_params import ImageItem
 from .model_rpc import VisualModelRpcServer
-from .visual_only_model_rpc import VisualOnlyModelRpcServer
 
 
 class VisualModelRpcClient:
     def __init__(self, rpc_conn):
-        self.rpc_conn: Union[VisualModelRpcServer, VisualOnlyModelRpcServer] = rpc_conn
+        self.rpc_conn: VisualModelRpcServer = rpc_conn
 
         def async_wrap(f):
             f = rpyc.async_(f)
@@ -31,7 +30,7 @@ class VisualModelRpcClient:
         ans: rpyc.AsyncResult = self._init_model(kvargs)
         await ans
         return
-    
+
     async def run_task(self, images: List[ImageItem], ref_event_list: List[threading.Event]):
         ans = self._run_task(images, ref_event_list)
         return await ans
