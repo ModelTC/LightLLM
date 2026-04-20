1f51c9e0b04c98f216b1320daecd916be3d2b1ff
wzj <wzjhelloworld@qq.com>
2026-04-20 14:17:30 +0000

    fix

--- lightllm/common/basemodel/triton_kernel/linear_att_copy.py
+++ lightllm/common/basemodel/triton_kernel/linear_att_copy.py
@@ -1,7 +1,6 @@
 import torch
 import triton
 import triton.language as tl
-from lightllm.common.triton_utils.autotuner import autotune
 
 
 @triton.jit
@@ -45,10 +44,11 @@ def _copy_linear_att_state_to_kv_buffer(
     if cur_seq_len % big_page_token_num != 0:
         return
 
-    cur_req_idx = tl.load(b_req_idx + cur_batch_size) * (mtp_step + 1)
+    cur_req_idx = tl.load(b_req_idx + cur_batch_size)
     req_to_mem_start = (
         req_to_token_mem_index + cur_req_idx * req_to_stride_idx + (cur_seq_len - big_page_token_num) * req_to_stride_s
     )
+    cur_state_req_idx = cur_req_idx * (mtp_step + 1)
 
     for i in range(tl.cdiv(gpu_conv_tail_dim, BLOCK)):
         gpu_start_i = i * BLOCK + tl.arange(0, BLOCK)
@@ -56,7 +56,7 @@ def _copy_linear_att_state_to_kv_buffer(
         conv_data = tl.load(
             gpu_conv_ptr
             + cur_layer * gpu_conv_stride_l
-            + cur_req_idx * gpu_conv_stride_s
+            + cur_state_req_idx * gpu_conv_stride_s
             + gpu_start_i * gpu_conv_stride_d,
             mask=mask,
         )
@@ -77,7 +77,7 @@ def _copy_linear_att_state_to_kv_buffer(
         ssm_data = tl.load(
             gpu_ssm_ptr
             + cur_layer * gpu_ssm_stride_l
-            + cur_req_idx * gpu_ssm_stride_s
+            + cur_state_req_idx * gpu_ssm_stride_s
             + gpu_start_i * gpu_ssm_stride_d,
             mask=mask,
         )
@@ -125,6 +125,154 @@ def copy_linear_att_state_to_kv_buffer(
         cpu_kv_ssm_ptr=cpu_kv_ssm_state,
         b_req_idx=b_req_idx,
         b_seq_len=b_seq_len,
+        req_to_token_mem_index=req_to_token_mem_index,
+        req_to_stride_idx=req_to_token_mem_index.stride(0),
+        req_to_stride_s=req_to_token_mem_index.stride(1),
+        gpu_conv_stride_l=gpu_conv_state.stride(0),
+        gpu_conv_stride_s=gpu_conv_state.stride(1),
+        gpu_conv_stride_d=gpu_conv_state.stride(2),
+        gpu_ssm_stride_l=gpu_ssm_state.stride(0),
+        gpu_ssm_stride_s=gpu_ssm_state.stride(1),
+        gpu_ssm_stride_d=gpu_ssm_state.stride(2),
+        cpu_kv_conv_stride_l=cpu_kv_conv_state.stride(0),
+        cpu_kv_conv_stride_s=cpu_kv_conv_state.stride(1),
+        cpu_kv_conv_stride_d=cpu_kv_conv_state.stride(2),
+        cpu_kv_ssm_stride_l=cpu_kv_ssm_state.stride(0),
+        cpu_kv_ssm_stride_s=cpu_kv_ssm_state.stride(1),
+        cpu_kv_ssm_stride_d=cpu_kv_ssm_state.stride(2),
+        mtp_step=mtp_step,
+        layer_num=layer_num,
+        big_page_token_num=big_page_token_num,
+        gpu_conv_tail_dim=gpu_conv_tail_dim,
+        gpu_ssm_tail_dim=gpu_ssm_tail_dim,
+        cpu_conv_tail_dim=cpu_conv_tail_dim,
+        cpu_ssm_tail_dim=cpu_ssm_tail_dim,
+        BLOCK=BLOCK,
+    )
+
+
+@triton.jit
+def _copy_kv_buffer_to_linear_att_state(
+    gpu_conv_ptr,  # [layer_num, size_num, xdim]
+    gpu_ssm_ptr,  # [layer_num, size_num, xdim]
+    cpu_kv_conv_ptr,  # [layer_num, size, xdim]
+    cpu_kv_ssm_ptr,  # [layer_num, size, xdim]
+    req_idx,  # int
+    seq_len,  # int
+    req_to_token_mem_index,  # [xxx, xxxx]
+    req_to_stride_idx,
+    req_to_stride_s,
+    gpu_conv_stride_l,
+    gpu_conv_stride_s,
+    gpu_conv_stride_d,
+    gpu_ssm_stride_l,
+    gpu_ssm_stride_s,
+    gpu_ssm_stride_d,
+    cpu_kv_conv_stride_l,
+    cpu_kv_conv_stride_s,
+    cpu_kv_conv_stride_d,
+    cpu_kv_ssm_stride_l,
+    cpu_kv_ssm_stride_s,
+    cpu_kv_ssm_stride_d,
+    mtp_step,
+    layer_num,
+    big_page_token_num,
+    gpu_conv_tail_dim,
+    gpu_ssm_tail_dim,
+    cpu_conv_tail_dim,
+    cpu_ssm_tail_dim,
+    BLOCK: tl.constexpr,
+):
+    cur_layer = tl.program_id(0)
+    cur_seq_len = seq_len
+    if cur_seq_len == 0:
+        return
+    if cur_seq_len % big_page_token_num != 0:
+        return
+
+    cur_req_idx = req_idx
+    cur_state_req_idx = cur_req_idx * (mtp_step + 1)
+
+    req_to_mem_start = (
+        req_to_token_mem_index + cur_req_idx * req_to_stride_idx + (cur_seq_len - big_page_token_num) * req_to_stride_s
+    )
+
+    for i in range(tl.cdiv(gpu_conv_tail_dim, BLOCK)):
+        gpu_start_i = i * BLOCK + tl.arange(0, BLOCK)
+        mask = gpu_start_i < gpu_conv_tail_dim
+        src_mem_index = tl.load(
+            req_to_mem_start + (gpu_start_i // cpu_conv_tail_dim) * req_to_stride_s, mask=mask, other=-11111111
+        )
+        src_conv_ptr = (
+            cpu_kv_conv_ptr
+            + cur_layer * cpu_kv_conv_stride_l
+            + src_mem_index * cpu_kv_conv_stride_s
+            + (gpu_start_i % cpu_conv_tail_dim) * cpu_kv_conv_stride_d
+        )
+        conv_data = tl.load(src_conv_ptr, mask=mask, other=0)
+        tl.store(
+            gpu_conv_ptr
+            + cur_layer * gpu_conv_stride_l
+            + cur_state_req_idx * gpu_conv_stride_s
+            + gpu_start_i * gpu_conv_stride_d,
+            conv_data,
+            mask=mask,
+        )
+
+    for i in range(tl.cdiv(gpu_ssm_tail_dim, BLOCK)):
+        gpu_start_i = i * BLOCK + tl.arange(BLOCK)
+        mask = gpu_start_i < gpu_ssm_tail_dim
+        src_mem_index = tl.load(
+            req_to_mem_start + (gpu_start_i // cpu_ssm_tail_dim) * req_to_stride_s, mask=mask, other=-11111111
+        )
+        src_conv_ptr = (
+            cpu_kv_ssm_ptr
+            + cur_layer * cpu_kv_ssm_stride_l
+            + src_mem_index * cpu_kv_ssm_stride_s
+            + (gpu_start_i % cpu_ssm_tail_dim) * cpu_kv_ssm_stride_d
+        )
+        ssm_data = tl.load(src_conv_ptr, mask=mask, other=0)
+        tl.store(
+            gpu_ssm_ptr
+            + cur_layer * gpu_ssm_stride_l
+            + cur_state_req_idx * gpu_ssm_stride_s
+            + gpu_start_i * gpu_ssm_stride_d,
+            ssm_data,
+            mask=mask,
+        )
+    return
+
+
+def copy_kv_buffer_to_linear_att_state(
+    req_idx: int,
+    seq_len: int,
+    req_to_token_mem_index: torch.Tensor,
+    gpu_conv_state: torch.Tensor,
+    gpu_ssm_state: torch.Tensor,
+    cpu_kv_conv_state: torch.Tensor,  # [layer_num, s, dim]
+    cpu_kv_ssm_state: torch.Tensor,  # [layer_num, s, xdim]
+    mtp_step: int,
+    big_page_token_num: int,
+):
+    BLOCK = 4096
+    gpu_conv_state = gpu_conv_state.view(gpu_conv_state.shape[0], gpu_conv_state.shape[1], -1).view(dtype=torch.uint8)
+    gpu_ssm_state = gpu_ssm_state.view(gpu_ssm_state.shape[0], gpu_ssm_state.shape[1], -1).view(dtype=torch.uint8)
+    gpu_conv_tail_dim = gpu_conv_state.shape[-1]
+    gpu_ssm_tail_dim = gpu_ssm_state.shape[-1]
+    cpu_conv_tail_dim = cpu_kv_conv_state.shape[-1]
+    cpu_ssm_tail_dim = cpu_kv_ssm_state.shape[-1]
+    layer_num = cpu_kv_conv_state.shape[0]
+    assert gpu_conv_state.shape[0] == gpu_ssm_state.shape[0] == cpu_kv_conv_state.shape[0] == cpu_kv_ssm_state.shape[0]
+
+    grid = (layer_num,)
+
+    _copy_linear_att_state_to_kv_buffer[grid](
+        gpu_conv_ptr=gpu_conv_state,
+        gpu_ssm_ptr=gpu_ssm_state,
+        cpu_kv_conv_ptr=cpu_kv_conv_state,
+        cpu_kv_ssm_ptr=cpu_kv_ssm_state,
+        req_idx=req_idx,
+        seq_len=seq_len,
         req_to_token_mem_index=req_to_token_mem_index,
         req_to_stride_idx=req_to_token_mem_index.stride(0),
         req_to_stride_s=req_to_token_mem_index.stride(1),
