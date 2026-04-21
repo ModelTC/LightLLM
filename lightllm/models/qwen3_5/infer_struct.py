4559032061741b921237b29ee66a1bf0f190b7e7
wangzaijun <wzjhelloworld@qq.com>
2026-04-21 08:41:23 +0000

    fix

--- lightllm/models/qwen3_5/infer_struct.py
+++ lightllm/models/qwen3_5/infer_struct.py
@@ -14,5 +14,9 @@ class Qwen35InferStateInfo(Qwen2VLInferStateInfo):
         super().init_some_extra_state(model)
         self.b_att_seq_len = self.b_seq_len
         mtp_step = get_env_start_args().mtp_step
-        self.b_buffer_idx = self.b_req_idx * (mtp_step + 1) + self.b_mtp_index
+
+        if self.is_prefill:
+            self.b_buffer_idx = self.b_req_idx * (mtp_step + 1)
+        else:
+            self.b_buffer_idx = self.b_req_idx * (mtp_step + 1) + self.b_mtp_index
         return
