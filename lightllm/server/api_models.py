ae3765886a5dc568ceeccb747348e47b560cf2a8
shihaobai <1798930569@qq.com>
2026-04-21 04:10:37 +0000

    add tool name check

--- lightllm/server/api_models.py
+++ lightllm/server/api_models.py
@@ -122,7 +122,7 @@ class CompletionRequest(BaseModel):
     prompt: Union[str, List[str], List[int], List[List[int]]]
     suffix: Optional[str] = None
     max_tokens: Optional[int] = Field(
-        default=16384, deprecated="max_tokens is deprecated, please use max_completion_tokens instead"
+        default=65536, deprecated="max_tokens is deprecated, please use max_completion_tokens instead"
     )
     max_completion_tokens: Optional[int] = None
     temperature: Optional[float] = 1.0
@@ -198,7 +198,7 @@ class ChatCompletionRequest(BaseModel):
     stream_options: Optional[StreamOptions] = None
     stop: Optional[Union[str, List[str]]] = None
     max_tokens: Optional[int] = Field(
-        default=16384, deprecated="max_tokens is deprecated, please use max_completion_tokens instead"
+        default=65536, deprecated="max_tokens is deprecated, please use max_completion_tokens instead"
     )
     max_completion_tokens: Optional[int] = None
     presence_penalty: Optional[float] = 0.0
