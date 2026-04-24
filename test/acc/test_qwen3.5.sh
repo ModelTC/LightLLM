3c630e910997aadb68ffabc195b9d5e3b42d291b
wangzaijun <wzjhelloworld@qq.com>
2026-04-24 10:02:54 +0000

    fix

--- test/acc/test_qwen3.5.sh
+++ test/acc/test_qwen3.5.sh
@@ -7,8 +7,11 @@ LOADWORKER=18 CUDA_VISIBLE_DEVICES=6,7 python -m lightllm.server.api_server \
 --port 8089
 
 # second
-export no_proxy="localhost,127.0.0.1,0.0.0.0,::1" HF_ALLOW_CODE_EVAL=1 HF_DATASETS_OFFLINE=0 lm_eval --model local-completions --model_args '{"model":"qwen/Qwen3.5-0.8B", "base_url":"http://localhost:8089/v1/completions", "max_length": 16384}' --tasks gsm8k --batch_size 500 --confirm_run_unsafe_code
-
+export no_proxy="localhost,127.0.0.1,0.0.0.0,::1"
+HF_ALLOW_CODE_EVAL=1 HF_DATASETS_OFFLINE=0 lm_eval  --model local-completions \
+--model_args '{"model":"Qwen/Qwen3.5-27B","base_url":"http://127.0.0.1:8089/v1/completions","tokenizer_backend":"huggingface","tokenizer":"qwen/Qwen3.5-0.8B","max_length":16384,"max_gen_toks":2048,"num_concurrent":8,"max_retries":3}' \
+--tasks gsm8k  \
+--batch_size 64
 
 # 测试
 LOADWORKER=18 CUDA_VISIBLE_DEVICES=6,7 python -m lightllm.server.api_server \
@@ -20,23 +23,28 @@ LOADWORKER=18 CUDA_VISIBLE_DEVICES=6,7 python -m lightllm.server.api_server \
 --linear_att_page_block_num 2 \
 --max_total_token_num 200000
 
-export no_proxy="localhost,127.0.0.1,0.0.0.0,::1" HF_ALLOW_CODE_EVAL=1 HF_DATASETS_OFFLINE=0 lm_eval --model local-completions --model_args '{"model":"qwen/Qwen3.5-0.8B", "base_url":"http://localhost:8089/v1/completions", "max_length": 16384}' --tasks gsm8k --batch_size 500 --confirm_run_unsafe_code
-
+export no_proxy="localhost,127.0.0.1,0.0.0.0,::1"
+HF_ALLOW_CODE_EVAL=1 HF_DATASETS_OFFLINE=0 lm_eval  --model local-completions \
+--model_args '{"model":"Qwen/Qwen3.5-27B","base_url":"http://127.0.0.1:8089/v1/completions","tokenizer_backend":"huggingface","tokenizer":"qwen/Qwen3.5-0.8B","max_length":16384,"max_gen_toks":2048,"num_concurrent":8,"max_retries":3}' \
+--tasks gsm8k  \
+--batch_size 64
 # 测试 cpu cache 与 linear att 的配合是否正常
-LOADWORKER=18 CUDA_VISIBLE_DEVICES=6,7 python -m lightllm.server.api_server \
---model_dir /root/.cache/huggingface/hub/models--Qwen--Qwen3.5-0.8B/snapshots/2fc06364715b967f1860aea9cf38778875588b17 \
---tp 2 \
+LOADWORKER=18 python -m lightllm.server.api_server \
+--model_dir /mtc/models/Qwen3.5-27B \
+--tp 8 \
 --port 8089 \
 --linear_att_cache_size 10 \
 --linear_att_hash_page_size 256 \
---linear_att_page_block_num 2 \
---max_total_token_num 20000 \
+--linear_att_page_block_num 4 \
+--max_total_token_num 60000 \
 --enable_cpu_cache  \
 --cpu_cache_storage_size 128
 
-export no_proxy="localhost,127.0.0.1,0.0.0.0,::1" HF_ALLOW_CODE_EVAL=1 HF_DATASETS_OFFLINE=0 lm_eval --model local-completions \
---model_args '{"model":"qwen/Qwen3.5-0.8B", "base_url":"http://localhost:8089/v1/completions", "max_length": 16384}' \
---tasks gsm8k --batch_size 500 --confirm_run_unsafe_code
+export no_proxy="localhost,127.0.0.1,0.0.0.0,::1"
+HF_ALLOW_CODE_EVAL=1 HF_DATASETS_OFFLINE=0 lm_eval  --model local-completions \
+--model_args '{"model":"Qwen/Qwen3.5-27B","base_url":"http://127.0.0.1:8089/v1/completions","tokenizer_backend":"huggingface","tokenizer":"qwen/Qwen3.5-0.8B","max_length":16384,"max_gen_toks":2048,"num_concurrent":8,"max_retries":3}' \
+--tasks gsm8k  \
+--batch_size 64
 
 
 # 帮我写一段提示词，告诉AI单独一个一个的进行上述测试的启动服务，然后再执行评测脚本，将结果写入out.txt 中，注意需要标记启动的参数和结果信息。不要用health 接口去判断服务是否启动，直接探测端口是否处于listen状态即可, 执行评测命令的时候，需要用no_proxy 将本地local ip 排除。
