3c242bec8d05c5bd9e34826dd599fc7cfad5acfe
wangzaijun <wzjhelloworld@qq.com>
2026-04-28 09:40:04 +0000

    fix

--- test/acc/test_qwen3.sh
+++ test/acc/test_qwen3.sh
@@ -25,7 +25,7 @@ LOADWORKER=18 CUDA_VISIBLE_DEVICES=6,7 python -m lightllm.server.api_server --mo
 export no_proxy="localhost,127.0.0.1,0.0.0.0,::1" HF_ALLOW_CODE_EVAL=1 HF_DATASETS_OFFLINE=0 lm_eval --model local-completions --model_args '{"model":"qwen/qwen3-8b", "base_url":"http://localhost:8089/v1/completions", "max_length": 16384}' --tasks gsm8k --batch_size 500 --confirm_run_unsafe_code
 
 
-LOADWORKER=18 CUDA_VISIBLE_DEVICES=6,7 python -m lightllm.server.api_server --model_dir /mtc/models/qwen3-8b --tp 2 --dp 2 --port 8089 --max_total_token_num 200000 --enable_cpu_cache  --cpu_cache_storage_size 4 --cpu_cache_token_page_size 128 --enable_disk_cache --disk_cache_storage_size 128 --disk_cache_dir /mtc/test/tmp/
+LOADWORKER=18 CUDA_VISIBLE_DEVICES=6,7 LOADWORKER=18 CUDA_VISIBLE_DEVICES=6,7 LIGHTLLM_DISK_CACHE_PROMPT_LIMIT_LENGTH=128 python -m lightllm.server.api_server --model_dir /mtc/models/qwen3-8b --tp 2 --dp 2 --port 8089 --max_total_token_num 200000 --enable_cpu_cache  --cpu_cache_storage_size 64 --cpu_cache_token_page_size 128 --enable_disk_cache --disk_cache_storage_size 256 --disk_cache_dir /mtc/test/tmp/
 
 export no_proxy="localhost,127.0.0.1,0.0.0.0,::1" HF_ALLOW_CODE_EVAL=1 HF_DATASETS_OFFLINE=0 lm_eval --model local-completions --model_args '{"model":"qwen/qwen3-8b", "base_url":"http://localhost:8089/v1/completions", "max_length": 16384}' --tasks gsm8k --batch_size 500 --confirm_run_unsafe_code
 
