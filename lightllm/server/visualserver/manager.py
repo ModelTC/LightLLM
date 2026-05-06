e7ed2c40b9e617ac582c466d45d5153660b90d30
sufubao <47234901+sufubao@users.noreply.github.com>
2026-05-06 19:35:04 +0800

    fix: honor visual infer batch size (#1293)

--- lightllm/server/visualserver/manager.py
+++ lightllm/server/visualserver/manager.py
@@ -84,7 +84,7 @@ class VisualManager:
                     "visual_nccl_port": self.args.visual_nccl_ports[dp_rank_id],
                     "quant_type": self.args.vit_quant_type,
                     "quant_cfg": self.args.vit_quant_cfg,
-                    "max_batch_size": min(self.infer_batch_size // self.vit_dp, 1),
+                    "max_batch_size": max(self.infer_batch_size // self.vit_dp, 1),
                     "visual_batch_max_tokens": self.args.visual_batch_max_tokens,
                     "vit_attn_backend": self.vit_attn_backend,
                 }
