e1f7b7736a194ae38d84458b4b74d008124b307a
wangzaijun <wzjhelloworld@qq.com>
2026-05-19 03:00:54 +0000

    fix

--- lightllm/server/router/model_infer/mode_backend/dynamic_mtp_planner.py
+++ lightllm/server/router/model_infer/mode_backend/dynamic_mtp_planner.py
@@ -1,3 +1,6 @@
+import random
+import math
+import numpy as np
 from typing import Dict, List, Optional
 
 
@@ -6,16 +9,25 @@ class DynamicMTPPlanner:
         self,
         mtp_step: int,
         ema_decay: float = 0.9,
-        confidence_k: float = 1.0,
+        use_random_mode: bool = True,
+        random_mode_iter_threshold: int = 100,
     ) -> None:
         self.mtp_step = mtp_step
-        self.confidence_k = confidence_k
+        self.ema_decay = ema_decay
+
         # 记录每个请求的 accept_len 的 ema 值， 用于分布统计
-        self.req_accept_len_ema = _EMAValue(ema_decay, init_value=float(self.mtp_step + 1))
-        self.req_accept_len_second_moment_ema = _EMAValue(ema_decay, init_value=float((self.mtp_step + 1) ** 2))
+        self.req_accept_len_ema = _EMAValue(self.ema_decay, init_value=float(self.mtp_step + 1))
+        self.req_accept_len_second_moment_ema = _EMAValue(self.ema_decay, init_value=float((self.mtp_step + 1) ** 2))
+
+        # 每多少个请求采用随机的方式决定 dynamic_batch_size
+        self._iter = 0
+        self._iter_threshold = random_mode_iter_threshold
+        self._use_random_mode = use_random_mode
+        random.seed(0)
 
         # 记录每个请求对应的单token速度记录信息
         self.req_num_to_speed_dict: Dict[int, List[_EMAValue]] = {}
+        return
 
     def update_req_accept_len_statics(self, accept_lens: List[int]) -> None:
         for accept_len in accept_lens:
@@ -32,14 +44,33 @@ class DynamicMTPPlanner:
 
     def get_dynamic_batch_size(self, req_num: int, original_batch_size: int) -> int:
         assert req_num * (self.mtp_step + 1) == original_batch_size
+        self._iter += 1
+        # case 1 如果采用随机的方式决定 dynamic_batch_size
+        if self._use_random_mode and self._iter % self._iter_threshold == 0:
+            sigma = math.sqrt(self.req_accept_len_second_moment_ema.get() - self.req_accept_len_ema.get() ** 2)
+            max_batch_size = min(req_num * (self.mtp_step + 1), int(self.req_accept_len_ema.get() + 1 * sigma))
+            return random.randint(req_num, max_batch_size)
 
-        mean = self.req_accept_len_ema.get()
-        return max(req_num, int(mean))
+        # case 2 如果采用统计的方式决定 dynamic_batch_size, 利用统计的 ema 信息来决定
+        ema_batch_size = max(req_num, int(self.req_accept_len_ema.get()))
+        sigma = math.sqrt(self.req_accept_len_second_moment_ema.get() - self.req_accept_len_ema.get() ** 2)
+        max_batch_size = min(req_num * (self.mtp_step + 1), int(self.req_accept_len_ema.get() + 1 * sigma))
+        start = req_num - req_num
+        end = max_batch_size - req_num
+        ema_index = ema_batch_size - req_num
+        speed_ema_list = self._get_req_num_speed_ema_list(req_num=req_num)
+        speeds = [obj.get() for obj in speed_ema_list[start : (end + 1)]]
+        # 对于期望均值的位置，我们稍微降低下其数据，便于在 ema 统计的数值不够多和准确的时候，更好的选中期望位置，
+        # 以获得平均性能。
+        speeds[ema_index] -= 0.001
+        min_index = np.argmin(speeds)
+        min_cost_batch_size = min_index + req_num
+        return min_cost_batch_size
 
     def _get_req_num_speed_ema_list(self, req_num: int) -> List["_EMAValue"]:
         if req_num not in self.req_num_to_speed_dict:
             self.req_num_to_speed_dict[req_num] = [
-                _EMAValue(self.ema_decay, init_value=10000000.0) for _ in range(req_num * (self.mtp_step))
+                _EMAValue(decay=self.ema_decay, init_value=10000000.0) for _ in range(req_num * (self.mtp_step))
             ]
         return self.req_num_to_speed_dict[req_num]
 
