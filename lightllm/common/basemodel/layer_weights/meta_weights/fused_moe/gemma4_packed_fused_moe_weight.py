6d7729f75c48ef288581c3b22c621015e232b73e
wangzaijun <wzjhelloworld@qq.com>
2026-05-21 02:50:43 +0000

    fix

--- lightllm/common/basemodel/layer_weights/meta_weights/fused_moe/gemma4_packed_fused_moe_weight.py
+++ lightllm/common/basemodel/layer_weights/meta_weights/fused_moe/gemma4_packed_fused_moe_weight.py
@@ -3,31 +3,36 @@ from lightllm.common.basemodel.layer_weights.meta_weights.fused_moe.fused_moe_we
 
 class Gemma4PackedFusedMoeWeight(FusedMoeWeight):
     def load_hf_weights(self, weights):
+        # 将权重名称的格式对齐基类的统一加载格式。
         gate_up_name = f"{self.weight_prefix}.gate_up_proj"
         down_name = f"{self.weight_prefix}.down_proj"
-
-        assert self.quant_method.method_name == "none", "Gemma-4 packed MoE currently supports bf16/no-quant weights."
         assert not self.enable_ep_moe, "Gemma-4 packed MoE currently supports TP mode only."
-
-        start = self.split_inter_size * self.tp_rank_
-        end = self.split_inter_size * (self.tp_rank_ + 1)
         moe_intermediate_size = self.moe_intermediate_size
 
         if gate_up_name in weights:
             gate_up_weight = weights[gate_up_name]
-            for expert_idx, local_expert_idx in self.expert_idx_to_local_idx.items():
-                gate_weight = gate_up_weight[expert_idx, start:end, :].contiguous()
-                up_weight = gate_up_weight[
-                    expert_idx, moe_intermediate_size + start : moe_intermediate_size + end, :
-                ].contiguous()
-                self.quant_method.load_weight(gate_weight, self.w1_list[local_expert_idx])
-                self.quant_method.load_weight(up_weight, self.w3_list[local_expert_idx])
+
+            for expert_idx in range(self.n_routed_experts):
+                expert_gate_weight = gate_up_weight[expert_idx, :moe_intermediate_size, :]
+                expert_up_weight = gate_up_weight[expert_idx, moe_intermediate_size:, :]
+
+                weights[
+                    f"{self.weight_prefix}.{expert_idx}.{self.w1_weight_name}.{self.quant_method.weight_suffix}"
+                ] = expert_gate_weight
+                weights[
+                    f"{self.weight_prefix}.{expert_idx}.{self.w3_weight_name}.{self.quant_method.weight_suffix}"
+                ] = expert_up_weight
+
+            del weights[gate_up_name]
 
         if down_name in weights:
             down_weight = weights[down_name]
-            for expert_idx, local_expert_idx in self.expert_idx_to_local_idx.items():
-                down_weight_slice = down_weight[expert_idx, :, start:end].contiguous()
-                self.quant_method.load_weight(down_weight_slice, self.w2_list[local_expert_idx])
+            for expert_idx in range(self.n_routed_experts):
+                expert_down_weight = down_weight[expert_idx, :, :]
+                weights[
+                    f"{self.weight_prefix}.{expert_idx}.{self.w2_weight_name}.{self.quant_method.weight_suffix}"
+                ] = expert_down_weight
+            del weights[down_name]
 
-        self._load_per_expert_scale(weights)
+        super().load_hf_weights(weights)
         return
