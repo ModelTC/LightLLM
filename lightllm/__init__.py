1ad981d06809631c1cd91fa8780440522e0894d1
WANDY666 <1060304770@qq.com>
2026-06-16 01:01:55 +0000

    restore

--- lightllm/__init__.py
+++ lightllm/__init__.py
@@ -1,31 +1,4 @@
 from lightllm.utils.device_utils import is_musa
 
-
-def _patch_mp_resource_tracker_for_semaphore():
-    from multiprocessing import resource_tracker
-
-    if getattr(resource_tracker, "_lightllm_ignore_semaphore", False):
-        return
-
-    orig_register = resource_tracker.register
-    orig_unregister = resource_tracker.unregister
-
-    def register(name, rtype):
-        if rtype == "semaphore":
-            return
-        return orig_register(name, rtype)
-
-    def unregister(name, rtype):
-        if rtype == "semaphore":
-            return
-        return orig_unregister(name, rtype)
-
-    resource_tracker.register = register
-    resource_tracker.unregister = unregister
-    resource_tracker._lightllm_ignore_semaphore = True
-
-
-_patch_mp_resource_tracker_for_semaphore()
-
 if is_musa():
     import torchada  # noqa: F401
