656a503b62c5c0f0b4768639b25f6c2e18f1afc9
wangzaijun <wzjhelloworld@qq.com>
2026-07-21 06:26:56 +0000

    fix

--- lightllm/server/multimodal_params.py
+++ lightllm/server/multimodal_params.py
@@ -77,10 +77,8 @@ class AudioItem:
         assert self._preload_data is not None
         ans = self._preload_data
         self._preload_data = None
-        return ans
-
-    def free(self):
         self._data = None
+        return ans
 
     def to_dict(self):
         ret = {}
