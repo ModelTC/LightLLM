e8f614f6f6a2b22de82d6421ace9e8687b3733db
shihaobai <1798930569@qq.com>
2026-07-29 09:26:20 +0000

    refactor(quant): keep quant type naming simple

--- lightllm/server/api_cli.py
+++ lightllm/server/api_cli.py
@@ -1,5 +1,4 @@
 import argparse
-from lightllm.common.quant_type import QUANT_TYPE_NONE, SUPPORTED_QUANT_TYPES, SUPPORTED_VIT_QUANT_TYPES
 
 
 def add_cli_args(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
@@ -612,8 +611,7 @@ def add_cli_args(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
     parser.add_argument(
         "--quant_type",
         type=str,
-        choices=SUPPORTED_QUANT_TYPES,
-        default=QUANT_TYPE_NONE,
+        default="none",
         help=(
             "Quantization methods (W = weight, A = activation):\n"
             "  quant_type                     quantization                           implementation backend\n"
@@ -657,8 +655,7 @@ def add_cli_args(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
     parser.add_argument(
         "--vit_quant_type",
         type=str,
-        choices=SUPPORTED_VIT_QUANT_TYPES,
-        default=QUANT_TYPE_NONE,
+        default="none",
         help="""Quantization method for ViT: w8a8 | fp8w8a8""",
     )
     parser.add_argument(
