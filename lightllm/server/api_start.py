d3e8ddd42b587f620cdd15b61d3436952367f520
sufubao <sufubao@sensetime.com>
2026-07-30 14:50:16 +0800

    fix: clean up shared memory on process exit

--- lightllm/server/api_start.py
+++ lightllm/server/api_start.py
@@ -27,6 +27,10 @@ from lightllm.utils.config_utils import (
 )
 from lightllm.utils.dist_check_utils import auto_configure_allreduce_flags_from_args
 from .startup_status import log_machine_status
+from lightllm.utils.auto_shm_cleanup import (
+    mark_registered_sysv_shm_for_deletion,
+    register_sysv_shm_for_cleanup,
+)
 
 logger = init_logger(__name__)
 
@@ -44,7 +48,7 @@ def setup_signal_handlers(http_server_process, process_manager):
             if http_server_process:
                 kill_recursive(http_server_process)
 
-            process_manager.terminate_all_processes()
+            process_manager.terminate_all_processes(graceful=False)
             logger.info("All processes have been forcefully terminated.")
             sys.exit(0)
         elif sig == signal.SIGTERM:
@@ -140,10 +144,13 @@ def normal_or_p_d_start(args):
     if args.enable_cpu_cache:
         # 生成一个用于创建cpu kv cache的共享内存id。
         args.cpu_kv_cache_shm_id = uuid.uuid1().int % 123456789
+        register_sysv_shm_for_cleanup(args.cpu_kv_cache_shm_id)
 
     if args.enable_multimodal:
         args.multi_modal_cache_shm_id = uuid.uuid1().int % 123456789
+        register_sysv_shm_for_cleanup(args.multi_modal_cache_shm_id)
 
+    setup_signal_handlers(None, process_manager)
     # 调度参数的自动设置, 人工设置则听人工的
     if args.router_token_ratio is None:
         if args.run_mode in ["normal"]:
@@ -502,6 +509,8 @@ def normal_or_p_d_start(args):
             (args,),
         ],
     )
+
+    mark_registered_sysv_shm_for_deletion()
 
     # 启动 Hypercorn
     command = [
