cb078e6da3bed99f56e79136598c13d2fe8a65e5
baishhao <baishihao@sensetime.com>
2026-08-24 20:02:05 +0800

    fix: restore image pixel limit default

--- lightllm/server/api_cli.py
+++ lightllm/server/api_cli.py
@@ -447,7 +447,7 @@ def add_cli_args(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
     parser.add_argument(
         "--max_image_pixels",
         type=int,
-        default=8294400,
+        default=3686400,  # 8294400 is 4k, 3686400 is 2k
         help="maximum allowed pixel count for one image before resize preprocessing",
     )
     parser.add_argument(
