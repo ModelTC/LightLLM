7d4e11d3f15b028ced2c0902885d61b86abd4a81
shihaobai <42648726+shihaobai@users.noreply.github.com>
2026-09-04 09:56:19 +0000

    refactor: reorganize request manager into package

---
+++ lightllm/common/req_manager/__init__.py
@@ -0,0 +1,5 @@
+from .base import ReqManager
+from .linear_att import ReqManagerForMamba
+from .req_sampling_params import ReqSamplingParamsManager
+
+__all__ = ["ReqManager", "ReqManagerForMamba", "ReqSamplingParamsManager"]
